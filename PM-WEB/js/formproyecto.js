7a0e8dc9b824f0e89e14dd96e2c7b574244f4be4
pinewood-uru <119970370+pinewood-uru@users.noreply.github.com>
2024-11-21 20:01:48 -0300

    f

--- PM-WEB/js/formproyecto.js
+++ PM-WEB/js/formproyecto.js
@@ -5,14 +5,26 @@ const btnsendproyecto = document.getElementById("form-proyecto-submit");
 
 btnsendproyecto.addEventListener("click", ()=>{
 
-const razonsocialInput = document.getElementById('razonsocial');
-const rutInput = document.getElementById('rut');
-const direccionInput = document.getElementById('direccion');
-const padronInput = document.getElementById('padron');
-const seccionalInput = document.getElementById('seccional');
-const barrioInput = document.getElementById('barrio');
-const guardarInfoCheckbox = document.getElementById('guardar-info');
-const formSubmitButton = document.getElementById('form-proyecto-submit');
+const razonsocialInput = document.getElementById("razonsocial");
+const rutInput = document.getElementById("rut");
+const direccionInput = document.getElementById("direccion");
+const padronInput = document.getElementById("padron");
+const seccionalInput = document.getElementById("seccional");
+const barrioInput = document.getElementById("barrio");
+const guardarInfoCheckbox = document.getElementById("guardar-info");
+const formSubmitButton = document.getElementById("form-proyecto-submit");
 
-Request.postproyecto
+if(!razonsocialInput || !rutInput || !direccionInput || !padronInput || !seccionalInput || !barrioInput || !guardarInfoCheckbox ||){
+    imprimir("form-register-error", "Completar todos los campos");
+    return;
+}
+
+const body = JSON.stringify({
+    razonsocioal,
+    rut,
+    direccion,
+    padron,
+    seccional,
+    barrio,
+    
 })
